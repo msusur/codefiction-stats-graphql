3b05e528f6ccec699e5d1b7a85804b22412db8a3
Mert Susur <mail@mertsusur.com>
2018-11-05 15:16:01 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+app.set('port', process.env.PORT || 4000);
+server.applyMiddleware({ app });
+app.listen(app.get('port'), () =>
+  console.log(
+    `Server ready at http://localhost:${app.get('port')}${server.graphqlPath}`
+  )
+);
