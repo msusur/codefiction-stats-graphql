1f8cd8e011f9486c1c8a1f1f7c186ab201a28937
msusur <mail@mertsusur.com>
2018-11-05 20:19:40 +0000

    Adding the react application

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
 const app = express();
 app.set('port', process.env.PORT || 4000);
 server.applyMiddleware({ app });
+app.use('/', express.static('build'));
 app.listen(app.get('port'), () =>
   console.log(
     `Server ready at http://localhost:${app.get('port')}${server.graphqlPath}`
