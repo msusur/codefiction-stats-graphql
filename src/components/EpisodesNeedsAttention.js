706f2233d94890d15a9a4aa29c5b42aae84c2aac
msusur <mail@mertsusur.com>
2018-11-06 22:49:55 +0000

    Adds filter for episodes

--- src/components/EpisodesNeedsAttention.js
+++ src/components/EpisodesNeedsAttention.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Button, Glyphicon } from 'react-bootstrap';
+import './EpisodesNeedsAttention.scss';
 
 export class EpisodesNeedsAttention extends Component {
+	state = { up: false };
+
 	render() {
 		const episodes = this.props.episodes;
 
 		return (
 			<div>
-				<label>En az dinlenen 10 Podcast bolumu</label>
+				<div>
+					<label>En az dinlenen 10 Podcast bolumu</label>
+				</div>
+				<div className="dashboard--button-container">
+					<Button bsSize="small" onClick={(event) => this.setState({ up: !this.state.up })}>
+						<Glyphicon glyph={this.state.up ? 'chevron-up' : 'chevron-down'} />{' '}
+						{this.state.up ? 'Cok' : 'Az'}
+					</Button>
+				</div>
 				<Table striped bordered condensed hover>
 					<thead>
 						<tr>
@@ -17,7 +28,12 @@ export class EpisodesNeedsAttention extends Component {
 					</thead>
 					<tbody>
 						{episodes
-							.sort((ep1, ep2) => ep1.stats.total_listens - ep2.stats.total_listens)
+							.sort(
+								(ep1, ep2) =>
+									this.state.up
+										? ep1.stats.total_listens - ep2.stats.total_listens
+										: ep2.stats.total_listens - ep1.stats.total_listens
+							)
 							.slice(0, 10)
 							.map((episode) => {
 								return (
