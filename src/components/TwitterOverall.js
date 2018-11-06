39fe8df5c73b4a41895548faf883533e1f975b66
Mert Susur <mail@mertsusur.com>
2018-11-06 17:22:43 +0000

    Validation on twitter null

--- src/components/TwitterOverall.js
+++ src/components/TwitterOverall.js
@@ -3,6 +3,9 @@ import './StatsOverall.scss';
 
 export class TwitterOverall extends Component {
   render() {
+    if (!this.props.value) {
+      return <div>Can't get twitter followers now.</div>;
+    }
     return (
       <div>
         Twitter followers
